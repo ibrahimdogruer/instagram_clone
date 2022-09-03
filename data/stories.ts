596a96faf1b1edc49cf878d17e73d3abf6939138
İbrahim Doğruer <ibrahim@assayazilim.com>
2022-09-03 21:36:02 +0300

    stories added

---
+++ data/stories.ts
@@ -0,0 +1,82 @@
+export default [
+  {
+    id: 1,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 2,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 3,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 4,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 5,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 6,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 7,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 8,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 9,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+  {
+    id: 10,
+    user: {
+      name: 'İbrahim Doğruer',
+      avatar:
+        'https://randomuser.me/api/portraits/women/28.jpg',
+    },
+  },
+];
--- src/screens/app/Home.tsx
+++ src/screens/app/Home.tsx
@@ -1,12 +1,14 @@
+import {Image, ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import {
   SvgHeart,
   SvgLogo,
   SvgMessenger,
   SvgNewPost,
 } from '../../components/svg-icons';
-import {Text, TouchableOpacity, View} from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
 import React from 'react';
+import stories from '../../../data/stories';
 
 const Home = () => {
   return (
@@ -36,8 +38,24 @@ const Home = () => {
       {/* end:: Header */}
 
       {/* begin:: Stories */}
-      <View>
-        <Text>Stories</Text>
+      <View className="py-3 border-b-[0.5px] border-[#dadada]">
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          {stories.map((story, i) => (
+            <View key={story.id} className="w-[67px] mx-2">
+              <LinearGradient
+                colors={['#DE0046', '#F7A34B']}
+                className="w-[65px] h-[65px] justify-center items-center rounded-full mb-2">
+                <Image
+                  className="w-[60px] h-[60px] rounded-full border-[3px] border-white"
+                  source={{
+                    uri: story.user.avatar,
+                  }}
+                />
+              </LinearGradient>
+              <Text className='text-xs' numberOfLines={1}>{story.user.name}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
       {/* end:: Stories */}
     </View>
